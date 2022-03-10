7c07f302528210746590cd6e946a1793fd071046
sohail <sohailss2010@gmail.com>
2022-03-10 16:10:57 +0530

    Updated employee payroll

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import PayrollForm from './components/payroll-form/payroll-form'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/payroll-form/home-page'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+          <Switch>
+            <Route exact path="/form" component={PayrollForm} />
+            <Route path="/form/:id" component={PayrollForm} />
+            <Route exact path="/home" component={Home} />
+          </Switch>
+        </Router>
     </div>
   );
 }
